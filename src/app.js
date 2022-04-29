e1592008f813e9926021206488549b316b417cdb
Ilya Koshelev <mifomen@yandex.ru>
2022-04-29 09:49:45 +0300

    change functions

--- src/app.js
+++ src/app.js
@@ -1,21 +1,7 @@
-import {setSecName, setMinName, setHourName, differenceTime} from './utils.js';
+import {setSecName, setMinName, setHourName, differenceTimes} from './utils.js';
 
 const nowTime = new Date();
 
-const setNameStepExam = (inputTime) => {
-  const getHours = inputTime.getHours();
-  const getMin = inputTime.getMinutes();
-  if ( getHours >= 9 && getHours < 10 && getMin >= 31 && getMin<= 59 ) {
-    return 'До начала экзамена';
-  }
-  if ( getHours >= 8 && getHours <= 9 && getMin>= 0 && getMin <= 30 ) {
-    return 'До получения ключа';
-  }
-  if ( getHours >= 10 && getHours <= 14 && getMin >= 0 && getMin <= 21 ) {
-    return 'До завершения экзамена';
-  }
-  return 'До следующего экзамена';
-};
 
 // document.querySelector('.time-name-hours').textContent=setHourName();
 const TimeForKey = new Date(2020, 3, 27, 9, 30, 0,  0);
@@ -37,4 +23,4 @@ const timerId = setInterval(() => {
 }, 1000);
 
 
-document.addEventListener('DOMContentLoaded',() => {  Timer();});
+document.addEventListener('DOMContentLoaded',() => {  differenceTimes();});
--- src/utils.js
+++ src/utils.js
@@ -36,46 +36,70 @@ const setSecName = (intInput) => {
 };
 
 
-const differenceTime = (TimeStart) => {
+const setNameStepExam = (inputTime) => {
+  const getHours = inputTime.getHours();
+  const getMin = inputTime.getMinutes();
+  if ( getHours >= 9 && getHours < 10 && getMin >= 31 && getMin<= 59 ) {
+    return 'До начала экзамена';
+  }
+  if ( getHours >= 8 && getHours <= 9 && getMin>= 0 && getMin <= 30 ) {
+    return 'До получения ключа';
+  }
+  if ( getHours >= 10 && getHours <= 14 && getMin >= 0 && getMin <= 21 ) {
+    return 'До завершения экзамена';
+  }
+  return 'До следующего экзамена';
+};
+
+// document.querySelector('.timer__title').textContent = setNameStepExam(today);
+// document.querySelector('.timer__description-sec').textContent=setSecName(tsec);
+// document.querySelector('.time-name-hours').textContent=setHourName(thour);
+// document.querySelector('.timer__description-min').textContent=setMinName(tmin);
+
+const differenceTimes = (timeEnd) => {
   let today = new Date();
   console.log(`setNameStepExam=${setNameStepExam(today)} today=${today}`); //eslint-disable-line
-  const timerTitle = document.querySelector('.timer__title').textContent = setNameStepExam(today);
 
-  today = Math.floor((TimeStart-today)/1000);
+
+  today = Math.floor((timeEnd-today)/1000);
   if (today < 1) {
     return true;
   } else {
     const tsec=today % 60;
-    document.querySelector('.timer__description-sec').textContent=setSecName(tsec);
     today=Math.floor(today / 60);
     const tmin=today % 60;
     today=Math.floor(today / 60);
     const thour=today % 24;
-    document.querySelector('.time-name-hours').textContent=setHourName(thour);
-
     today=Math.floor(today/24);
-    document.querySelector('.timer__description-min').textContent=setMinName(tmin);
 
-    if ( (TimeStart-today) % 60 === 0) {
-      document.querySelector('.first-collum').classList.add('vh');
-      document.querySelectorAll('.days').forEach( (element) => {
-        element.classList.add('fz72px');
-      });
+    if ( (timeEnd-today) % 60 === 0) {
+
     }
-    document.querySelector('#day').innerHTML=today;
+    document.querySelector('.js-time-now-days').innerHTML=today;
     // if (true) {
     // document.querySelector('.timer__day').classList.add('vh');
     // document.querySelector('.first-collum').classList.add('vh');
     // }
-    document.querySelector('#hour').innerHTML=thour;
+    document.querySelector('.js-time-now-hours').innerHTML=thour;
 
     if (thour === 0) {
       document.querySelector('.timer__column--hour').classList.add('vh');
     }
-    document.querySelector('#min').innerHTML=tmin;
-    document.querySelector('#sec').innerHTML=tsec;
-    window.setTimeout(Timer(timeEnd),10000);
+    document.querySelector('.js-time-now-mins').innerHTML=tmin;
+    document.querySelector('.js-time-now-seconds').innerHTML=tsec;
+    // window.setTimeout(differenceTimes(timeEnd),10000);
   }
 };
 
-export { setSecName, setMinName, setHourName, differenceTime };
+if (document.querySelector('.first-collum')) {
+  document.querySelector('.first-collum').classList.add('vh');
+}
+if (document.querySelector('.days')) {
+  document.querySelectorAll('.days').forEach( (element) => {
+    element.classList.add('fz72px');
+  });
+
+}
+
+
+export { setSecName, setMinName, setHourName, differenceTimes };
